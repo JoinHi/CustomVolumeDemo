8afbfc06b5291e02762fc1ddd1ae43968e463ff9
zzj_jie <zzj2202@163.com>
2016-11-30 00:14:09 +0800

    222

--- app/src/main/java/com/zzj/customdemo/MainActivity.java
+++ app/src/main/java/com/zzj/customdemo/MainActivity.java
@@ -7,20 +7,9 @@ import android.widget.Toast;
 import com.zzj.customvoice.CustomRadio;
 
 public class MainActivity extends AppCompatActivity {
-<<<<<<< HEAD
 //增加一行注11
 
-//bendizengjia
-//第三
-=======
-<<<<<<< HEAD
 
-//bendizengjia
-=======
-//电脑修改
-//电脑增加的内容
->>>>>>> a7d93d957f1af927821a858c404241d79be19580
->>>>>>> 7736994c634658a569595f6725d39b1b1f19d30a
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
